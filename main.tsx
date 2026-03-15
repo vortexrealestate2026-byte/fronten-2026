f2afb7387711dbfde76c6cf67a331657c360da7d
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-15 01:14:08 -0500

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router";
+import AppRouter from "./router.tsx";
 import "./styles/globals.css";
 
 const rootElement = document.getElementById("root");
